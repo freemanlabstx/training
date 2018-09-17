d275566ebd71e32d1b6d9881e936f9657c92df8c
Kristian Freeman <kristian@kristianfreeman.com>
2018-09-17 11:39:03 -0700

    Bugfix

--- javascript/array-tests/array-tests.js
+++ javascript/array-tests/array-tests.js
@@ -16,6 +16,6 @@ assert.equal(citiesString, "Phoenix, Los Angeles, Seattle")
 // No changes are needed below this line.
 assert.notEqual(cities.length, 0, "The cities variable should have at least one object inside of it");
 assert.notEqual(cities.length, 1, "Try defining more than one city object inside of the cities array");
-assert.equal(cities.find(c => c === "Phoenix", "Phoenix", "The cities array should include Phoenix");
+assert.equal(cities.find(c => c === "Phoenix"), "Phoenix", "The cities array should include Phoenix");
 
 console.log("Passed! :)");
