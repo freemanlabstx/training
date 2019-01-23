6da81a168e98be39ec6f668cb15f3ae244d7be7a
Kristian Freeman <kristian@kristianfreeman.com>
2019-01-23 15:53:15 -0600

    Update css-classes exercise with more to do

--- react/css-classes-styles/src/App.js
+++ react/css-classes-styles/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
-import './App.css';
+import React, { Component } from "react";
+import classnames from "classnames";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -24,16 +24,13 @@ class App extends Component {
      * Pass this style object as the style of the p tag below.
      */
 
-     /*
-      * Finally, fix the following classNames function below,
-      * so that the properties below are true:
-      *   - bg-red is true
-      *   - largePadding is true _if_ style.lineHeight is greater than 1
-      */
-    const divClasses = classnames({
-      'bg-red': false,
-      largePadding: style.linHeight > 1
-    })
+    /*
+     * Finally, fix the following classNames function below,
+     * so that the properties below are true:
+     *   - bg-red is true
+     *   - largePadding is true _if_ style.lineHeight is greater than 1
+     */
+    const divClasses = classnames({});
 
     return (
       <div className={divClasses}>
